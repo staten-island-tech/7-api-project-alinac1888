df02966be7a8e62757081c9f763d3e23f8c22fdd
alinac1888 <alinac44@nycstudents.net>
2025-12-12 06:26:06 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
 import requests
-requests.get(f"https://api.nookipedia.com/")
+
+def get(villager):
+    response = requests.get(f"9681c95b-6735-4bac-97fa-8d2f95d52242"(villager.lower()))
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return {
+        "villager": data["villager"]
+    }
+
+villager = get("Daisy")
+print(villager)
