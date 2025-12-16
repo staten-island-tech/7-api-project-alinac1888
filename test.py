6104d8e94f8cad17ff40b66343efcff1c5853f92
alinac1888 <alinac44@nycstudents.net>
2025-12-16 05:36:48 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 import requests
 
 def get(villager):
-    response = requests.get(f"9681c95b-6735-4bac-97fa-8d2f95d52242"(villager.lower()))
+    url = "https://api.animalcrossing.com/data"
+    key = "9681c95b-6735-4bac-97fa-8d2f95d52242"
+    headers = {
+    "Authorization": f"Bearer {key}"
+    }
+    response = requests.get(f"api.nookipedia.com"(villager.lower()))
     if response.status_code != 200:
         print("Error fetching data!")
         return None
