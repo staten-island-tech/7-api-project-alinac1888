5baa702007c739c518b4f46f881f9f608c8524db
alinac1888 <alinac44@nycstudents.net>
2025-12-16 06:12:49 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,17 @@
 import requests
+headers = {
+    "X-API-KEY": "9681c95b-6735-4bac-97fa-8d2f95d52242",
+    "Accept-Version": "1.7.0"
+}
+def get():
 
-def get(villager):
-    url = "https://api.animalcrossing.com/data"
-    key = "9681c95b-6735-4bac-97fa-8d2f95d52242"
-    headers = {
-    "Authorization": f"Bearer {key}"
-    }
-    response = requests.get(f"api.nookipedia.com"(villager.lower()))
+    url = "https://api.nookipedia.com/villagers"
+    response = requests.get(url, headers=headers)
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     
     data = response.json()
-    return {
-        "villager": data["villager"]
-    }
+    print(data)
 
-villager = get("Daisy")
-print(villager)
+get()
