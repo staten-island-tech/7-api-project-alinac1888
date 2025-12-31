0a5eac725d63a31d2c1a2d8a34437cf472662e15
alinac1888 <alinac44@nycstudents.net>
2025-12-30 19:00:22 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,36 +3,30 @@ headers = {
     "X-API-KEY": "9681c95b-6735-4bac-97fa-8d2f95d52242",
     "Accept-Version": "1.7.0"
 }
-def get():
 
-    url = "https://api.nookipedia.com/villagers"
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    
-    url = "https://api.nookipedia.com/nh/fish"
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    
-    data = response.json()
-    return {
-        "name": data["name"],
-        "species": data["species"],
-        "personality": data["personality"],
-        "phrase": data["phrase"]
-            }
-get() 
 class villager:
-    def _init_(e, vill, inv, fishamoun, sea):
+    def __init__(e, vill, inv, fishamoun, sea):
         e.vill = vill
         e.inv = inv
         e.fishamoun = fishamoun
         e.sea = sea
         sea = 10
         inv = []
+
+    def get():
+        url = "https://api.nookipedia.com/villagers"
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            print("Error fetching data!")
+            return None
+    
+        data = response.json()
+        return {
+        "name": data["name"],
+        "species": data["species"],
+        "personality": data["personality"],
+        "phrase": data["phrase"]
+            }
 
     def start(e, data):
         print(data["name"])
