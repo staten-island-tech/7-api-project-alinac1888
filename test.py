ff22c8f809222f114ad4d8969803d352bad2821c
alinac1888 <alinac44@nycstudents.net>
2026-01-02 11:55:40 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,14 +7,15 @@ import tkinter as tk
 import random
 
 class villager:
-    def __init__(e, inv):
+    def __init__(e, inv, fishamoun):
         e.vill = None
         e.inv = inv
         inv = []
         e.personality = None
+        e.fish_amoun = fishamoun
 
     def start(e):
-        star = input("wanna start? y/n")
+        star = input("wanna start? y/n").lower()
         if star == "y":
             url = "https://api.nookipedia.com/villagers"
             response = requests.get(url, headers=headers)
@@ -22,13 +23,20 @@ class villager:
                 print("Error fetching data!")
 
             data = response.json()
+
+            villager_n = []
+
             for villager in data:
-                print(f"name: {villager['name']} - personality: {villager['personality']}")
+                name = villager["name"]
+                villager_n.append(name)
+                print(f"name: {name} - species: {villager['species']} - personality: {villager['personality']}")
             while True:
                 choice = input("what villager")
-                if choice == villager:
+                if choice in villager_n:
                     print(f"{choice} yay")
                     e.vill = choice
+                    e.personality = {villager['personality']}
+                    e.fishing()
                     break
                 else:
                     print("sorry its not a villager")
@@ -38,12 +46,20 @@ class villager:
             e.start()
 
     def fishing(e):
-        import random
-        if e.fishamoun > 0:
-            random.choice(e.loot)
-            print(f"you caught a {e.loot}")
-        if e.fishamoun <= 0:
-            print("sorry your line broke")
+        fish = ['tuna', 'salmon', 'sea bass', 'boot', 'squid', 'goldfish', 'red carp', 'fish poop', 'pike']
+        chances = [2, 1, 1, 0, 2, 1, 1, 0, 2000]
+        bigchoice = input("Fish? y/n").lower()
+        while bigchoice == "y":
+            if e.fish_amoun > 0:
+                catch = random.choice(fish)
+                print(f"You caught a {catch}!")
+                catch += e.fish_amoun
+                break
+            else:
+                print("your line broke")
+        if bigchoice == "n":
+            print("say y when ur ready")
+            e.fishing()
 
-vill = villager([])
+vill = villager([], 10)
 vill.start()
