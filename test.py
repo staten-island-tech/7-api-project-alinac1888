db5790391cc54030e5794e75c6b635cc1ae196fb
alinac1888 <alinac44@nycstudents.net>
2026-01-07 05:55:56 -0800

    Update test.py

--- test.py
+++ test.py
@@ -6,77 +6,77 @@ headers = {
 import tkinter as tk
 from tkinter import messagebox
 import random
-from PIL import Image, ImageTk
+# from PIL import Image, ImageTk
 
-# class villager:
-#     def __init__(e, inv, fishamoun):
-#         e.vill = None
-#         e.inv = inv
-#         inv = []
-#         e.personality = None
-#         e.fish_amoun = fishamoun
+class villager:
+    def __init__(e, inv, fishamoun):
+        e.vill = None
+        e.inv = inv
+        inv = []
+        e.personality = None
+        e.fish_amoun = fishamoun
 
-#     def start(e):
-#         star = input("wanna start? y/n").lower()
-#         if star == "y":
-#             url = "https://api.nookipedia.com/villagers"
-#             response = requests.get(url, headers=headers)
-#             if response.status_code != 200:
-#                 print("Error fetching data!")
+    def start(e):
+        star = input("wanna start? y/n").lower()
+        if star == "y":
+            url = "https://api.nookipedia.com/villagers"
+            response = requests.get(url, headers=headers)
+            if response.status_code != 200:
+                print("Error fetching data!")
 
-#             data = response.json()
+            data = response.json()
 
-#             villager_n = []
+            villager_n = []
 
-#             for villager in data:
-#                 name = villager["name"]
-#                 villager_n.append(name)
-#                 print(f"name: {name} - species: {villager['species']} - personality: {villager['personality']}")
-#             while True:
-#                 choice = input("what villager")
-#                 if choice in villager_n:
-#                     print(f"{choice} yay")
-#                     e.vill = choice
-#                     e.personality = {villager['personality']}
-#                     e.fishing()
-#                     break
-#                 else:
-#                     print("sorry its not a villager")
+            for villager in data:
+                name = villager["name"]
+                villager_n.append(name)
+                print(f"name: {name} - species: {villager['species']} - personality: {villager['personality']}")
+            while True:
+                choice = input("what villager")
+                if choice in villager_n:
+                    print(f"{choice} yay")
+                    e.vill = choice
+                    e.personality = {villager['personality']}
+                    e.fishing()
+                    break
+                else:
+                    print("sorry its not a villager")
                         
-#         elif star == "n":
-#             print("say y when ur ready")
-#             e.start()
-#         else:
-#             print("inavlid option")
-#             e.start()
+        elif star == "n":
+            print("say y when ur ready")
+            e.start()
+        else:
+            print("inavlid option")
+            e.start()
 
-#     def choose_fish(e, fish, chances):
-#         return random.choices(fish, weights=chances, k=1)[0]
+    def choose_fish(e, fish, chances):
+        return random.choices(fish, weights=chances, k=1)[0]
     
-#     def fishing(e):
-#         fish = ['tuna', 'salmon', 'sea bass', 'boot', 'squid', 'goldfish', 'red carp', 'fish poop', 'pike']
-#         chances = [1, 3, 3, 5, 2, 4, 4, 5, 1]
-#         bigchoice = input("Fish? y/n").lower()
-#         while bigchoice == "y":
-#             if e.fish_amoun > 0:
-#                 catch = e.choose_fish(fish, chances)
-#                 print(f"{e.vill} caught a {catch}!")
-#                 e.inv.append(catch)
-#                 e.fish_amoun -= 1
-#             elif e.fish_amoun == 0:
-#                 print(f"{e.vill}'s line broke, here's all the fish you've caught: {e.inv}")
-#                 break
+    def fishing(e):
+        fish = ['tuna', 'salmon', 'sea bass', 'boot', 'squid', 'goldfish', 'red carp', 'fish poop', 'pike']
+        chances = [1, 3, 3, 5, 2, 4, 4, 5, 1]
+        bigchoice = input("Fish? y/n").lower()
+        while bigchoice == "y":
+            if e.fish_amoun > 0:
+                catch = e.choose_fish(fish, chances)
+                print(f"{e.vill} caught a {catch}!")
+                e.inv.append(catch)
+                e.fish_amoun -= 1
+            elif e.fish_amoun == 0:
+                print(f"{e.vill}'s line broke, here's all the fish you've caught: {e.inv}")
+                break
 
-#             bigchoice = input("Fish? y/n").lower()
-#         if bigchoice == "n":
-#             print("say y when ur ready")
-#             e.fishing()
-#         else: 
-#             print("invalid choice try again")
-#             e.fishing()
+            bigchoice = input("Fish? y/n").lower()
+        if bigchoice == "n":
+            print("say y when ur ready")
+            e.fishing()
+        else: 
+            print("invalid choice try again")
+            e.fishing()
 
-# vill = villager([], 10)
-# vill.start()
+vill = villager([], 10)
+vill.start()
 
 # class Villager:
 #     def __init__(self, fishamoun=10):
